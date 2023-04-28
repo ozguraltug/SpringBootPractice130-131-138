493b4a70a98e5a390849c648befd7c3d55fde52f
Ozgur Altug <ozguraltug06@gmail.com>
2023-04-28 20:19:18 +0300

    p2

--- SpringBootPrc/src/main/java/com/tpe/controller/CustomerControler.java
+++ SpringBootPrc/src/main/java/com/tpe/controller/CustomerControler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
@@ -20,18 +21,43 @@ public class CustomerControler {
 
     //1-Spring boot u selamlama http://localhost:8080/customers/greet
     @GetMapping("/greet")
-    public String greetSpringBoot(){
+    public String greetSpringBoot() {
         return "Hello Spring Boot";
     }
 
     //2-Customer oluşturma/kaydetme http://localhost:8080/customers/save
     @PostMapping("/save")
-    public ResponseEntity<String> saveCustomer(@Valid @RequestBody Customer customer){
+    public ResponseEntity<String> saveCustomer(@Valid @RequestBody Customer customer) {
         customerService.saveCustomer(customer);
-        String message="Customer saved successfully";
+        String message = "Customer saved successfully";
         return new ResponseEntity<>(message, HttpStatus.CREATED);
-
-
     }
 
+    //3-Tüm customerları getirme->http://localhost:8080/customers
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomer() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+
+    //4-Id ile customer getirme->http://localhost:8080/customers/1
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("id") Long id) {
+        Customer customer = customerService.getCustomerById(id);
+        return ResponseEntity.ok(customer);
+    }
+
+    //5-id ile customer getirme->http://localhost:8080/customers/custom?id=1
+    @GetMapping("/custom")
+    public ResponseEntity<Customer> getCustomerById2(@RequestParam("id") Long id) {
+        Customer customer = customerService.getCustomerById(id);
+        return ResponseEntity.ok(customer);
+    }
+    // 6-id ile customer silme->http://localhost:8080/customers/delete/1
+    //Customer is deleted successfully mesajı dönsün
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable("id") Long id){
+        customerService.deleteCustomer(id);
+        return ResponseEntity.ok("Customer is deleted successfully");
+    }
 }
--- SpringBootPrc/src/main/java/com/tpe/service/CustomerService.java
+++ SpringBootPrc/src/main/java/com/tpe/service/CustomerService.java
@@ -1,10 +1,43 @@
 package com.tpe.service;
 
 import com.tpe.domain.Customer;
+import com.tpe.exception.ConflictException;
+import com.tpe.exception.ResourceNotFoundException;
+import com.tpe.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
     public void saveCustomer(Customer customer) {
+
+        boolean isExistCustomer = customerRepository.existsByEmail(customer.getEmail());
+        if (isExistCustomer) {
+            throw new ConflictException("Customer already exist by email:" + customer.getEmail());
+        }
+        customerRepository.save(customer);
+
+    }
+
+    public List<Customer> getAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+        return customers;
+    }
+
+    public Customer getCustomerById(Long id) {
+       Customer customer = customerRepository.
+               findById(id).orElseThrow(()->new ResourceNotFoundException("Customer not found by id "+id));
+   return customer;
+    }
+
+    public void deleteCustomer(Long id) {
+       Customer customer= getCustomerById(id);
+       customerRepository.delete(customer);
     }
 }
